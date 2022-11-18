91fe56525a0f50edec1b389114e79b587d994c0f
obapythonaire <abdulahogundare@gmail.com>
2022-11-18 11:46:03 +0100

    task 49

---
+++ python/49program to convert hours to seconds.py
@@ -0,0 +1,14 @@
+# program to convert hours to seconds
+
+Hours = int(input("Enter the number in hours: "))
+Seconds = Hours*3600
+
+print(f"{Hours}hrs in seconds is {Seconds}secs")
+
+
+# program to convert seconds to hours
+
+Seconds = int(input("Enter the number in hours: "))
+Hours = Seconds/3600
+
+print(f"{Hours}hrs in seconds is {Seconds}secs")

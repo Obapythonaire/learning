0e6c294c3492b3801e7dbe610b9a05d38c370a8e
obapythonaire <abdulahogundare@gmail.com>
2022-11-18 11:23:07 +0100

    18 November 2022, 11:23am

---
+++ python/47findsquaresandsumofsquares of two nums.py
@@ -0,0 +1,33 @@
+# program to get 2 numbers from user, find
+# the square, add the squares and display the result.
+
+# import math
+
+# num=[]
+# x = int(input("Enter the first value: "))
+# y = int(input("Enter the first value: "))
+# sq_x = math.pow(x, 2)
+# sq_y = math.pow(y, 2)
+# num.append(sq_x)
+# num.append(sq_y)
+# total = sum(num)
+
+# print(f"The squares of {x} and {y} are: {sq_x}, {sq_y} and the sum of their squares is {total}")
+
+
+# ASSIGNMENT
+# program to get 2 numbers from user, find
+# the cube, add the squares and display the result.
+
+import math
+
+num=[]
+x = int(input("Enter the first value: "))
+y = int(input("Enter the first value: "))
+sq_x = math.pow(x, 3)
+sq_y = math.pow(y, 3)
+num.append(sq_x)
+num.append(sq_y)
+total = sum(num)
+
+print(f"The cubes of {x} and {y} are: {sq_x}, {sq_y} and the sum of their cubes is {total}")
---
+++ python/48 swap two numbers.py
@@ -0,0 +1,23 @@
+# program to get two numbers from user,
+# swap and display the new result
+
+# x = int(input("Enter a number: "))
+# y = int(input("Enter a number: "))
+# x_bs = x
+# y_bs = y
+# temp = x
+# x=y
+# y = temp
+# print(f"The values are {x_bs} and {y_bs} before swapping and {x} and {y} after swapping ")
+
+
+# ASSIGNMENT
+# program to get two numbers from user,
+# swap and display the new result
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+x_bs = x
+y_bs = y
+x, y = y,x
+print(f"The values are {x_bs} and {y_bs} before swapping and {x} and {y} after swapping ")

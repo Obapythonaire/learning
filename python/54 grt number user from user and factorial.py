1e4acd98eb5c9c2fe5ae5e33c637268c7bc4da16
obapythonaire <abdulahogundare@gmail.com>
2022-11-21 14:40:31 +0100

    factorial

---
+++ python/54 grt number user from user and factorial.py
@@ -0,0 +1,18 @@
+# program to get number user from user and factorial
+
+# num = int(input("Enter a number: "))
+# factorial = 1
+# for i in range(1, num+1):
+#     factorial*=i
+
+# print(f"The factorial of {num} is {factorial}.")
+
+# ASSIGNMENT
+# program to get number user from user and factorial
+import math
+num = int(input("Enter a number: "))
+factorial = math.factorial(num)
+
+print(f"The factorial of {num} is {factorial}.")
+
+

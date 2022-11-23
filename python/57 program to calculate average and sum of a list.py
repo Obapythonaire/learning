c739ba17a2c605ea839c71f40e2ec11f1c3427df
obapythonaire <abdulahogundare@gmail.com>
2022-11-23 14:44:42 +0100

    program to calculate average and sum of a list

---
+++ python/57 program to calculate average and sum of a list.py
@@ -0,0 +1,36 @@
+# program to calculate average
+
+# cont = []
+# high_num = int(input("Enter the amount of number needed: "))
+
+# for i in range(1, high_num+1):
+#     if i == 1:
+#         cont.append(int(input(f"Enter the {i}st number: ")))
+#     elif i == 2:
+#         cont.append(int(input(f"Enter the {i}nd number: ")))
+#     elif i == 3:
+#         cont.append(int(input(f"Enter the {i}rd number: ")))
+#     elif i == 4:
+#         cont.append(int(input(f"Enter the {i}th number: ")))
+#     elif i == 5:
+#         cont.append(int(input(f"Enter the {i}th number: ")))
+#     else:
+#         cont.append(int(input(f"Enter the {i}th number: ")))
+
+# total = sum(cont)
+# avg = total/len(cont)
+
+# print(f" The list is {cont} with sum of {total} and average of {avg}")
+
+# Assignment
+# program to convert binary to decimal
+
+binary = (input("Enter a number: "))
+conv = int(binary, 2)
+
+print(f"{binary} in decimal is {conv}")
+
+binary = (input("Enter a number: "))
+conv = int(binary, 10)
+
+print(f"{binary} in decimal is {conv}")

7a4f092331eaf84f666f660fb123d1d5460ce845
obapythonaire <abdulahogundare@gmail.com>
2022-11-23 14:43:58 +0100

    program to calculate percentage

---
+++ python/56 program to calculate percentage.py
@@ -0,0 +1,20 @@
+# program to calculate percentage
+# main_num = float(input("Enter a number: "))
+# perc_to_calc = float(input("Enter a number: "))
+
+# per = main_num/perc_to_calc * 100
+
+# print(f"The percentage of {main_num} in {perc_to_calc} is {per}%.")
+
+
+# Assignment
+# program to convert decimal to binary
+
+dec = int(input("Enter a number: "))
+conv = bin(dec)[2:]
+
+print(f"{dec} in binary is {conv}")
+# conv = dec//2
+# if conv == 1 or conv ==2:
+#     print(conv, end='')
+# # print(conv)

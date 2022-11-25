52c97a91926306faa029005863f16f0b1aea45d1
obapythonaire <abdulahogundare@gmail.com>
2022-11-25 13:49:43 +0100

    get first and last name and add

---
+++ python/58  program to get first and last name and add.py
@@ -0,0 +1,15 @@
+# program to get first and last name
+# and out them together
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+# print(f"Well done {first_name} {last_name}.")
+
+# program to get first and last name
+# and out them together and capitalize
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+y= first_name.upper()
+x = last_name.capitalize()
+print(f"Well done {y} {x}.")

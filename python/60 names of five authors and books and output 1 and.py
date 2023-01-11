a23c972cf52db765d1d2c51df1fc87e3db18fc02
obapythonaire <abdulahogundare@gmail.com>
2023-01-11 13:35:48 +0100

    changes n0 11/1/2023

--- python/60 names of five authors and books and output 1 and.py
+++ python/60 names of five authors and books and output 1 and.py
@@ -1,10 +1,12 @@
 # program to get names of five authors
 # and book
 book_author= {}
-for i in range(2):
+for i,j in book_author(2):
     author = input("Enter name of author: ")
     book_name= input("Enter book name: ")
-    book_author[author] = book_name
-last_author = (list(book_author.keys()[-1]))
-last_book = (list(book_author.values()[-1]))
-print(f"Last author is{last_author} and book is {last_book}.")
+    book_author.update({"{author}": "{book}"})
+    # book_author[author] = book_name
+# last_author = (list(book_author.keys()[-1]))
+# last_book = (list(book_author.values()[-1]))
+# print(f"Last author is{last_author} and book is {last_book}.")
+print(book_author)
---
+++ python/61 program to convert GB to Bytes.py
@@ -0,0 +1,4 @@
+# program to convert GB to Bytes
+
+Gigabyte = int(input(("Enter the Gigabyte: ")))
+to_bytes = Gigabyte/

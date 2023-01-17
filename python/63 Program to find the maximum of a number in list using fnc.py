76643e72007bdbb48b4cbf8387ca74fd84794ae8
obapythonaire <abdulahogundare@gmail.com>
2023-01-17 11:26:55 +0100

    max and min no in a list in function

---
+++ python/63 Program to find the maximum of a number in list using fnc.py
@@ -0,0 +1,23 @@
+# Program to find the maximum of a number in a list
+# using function
+# numbers =[]
+# def max_no():
+#     for num in range(5):
+#         numbers.append(int(input(f"Enter number {num}: ")))
+#         max_no = max(numbers)
+#         # return max_no
+#     print(f" {numbers} and the maximum no is {max_no}")
+# max_no()
+
+
+# Assignment
+# Program to find the minimum of a number in a list
+# using function
+numbers =[]
+def min_no():
+    for num in range(1, 6):
+        numbers.append(float(input(f"Enter number {num}: ")))
+        min_no = min(numbers)
+        # return max_no
+    print(f" {numbers} and the maximum no is {min_no}")
+min_no()

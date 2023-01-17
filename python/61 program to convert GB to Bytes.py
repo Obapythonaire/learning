f41d761b95a48f8ff431a6f27d53d7050784facc
obapythonaire <abdulahogundare@gmail.com>
2023-01-17 11:25:49 +0100

    convert gb to Bytes

--- python/61 program to convert GB to Bytes.py
+++ python/61 program to convert GB to Bytes.py
@@ -1,4 +1,12 @@
 # program to convert GB to Bytes
 
-Gigabyte = int(input(("Enter the Gigabyte: ")))
-to_bytes = Gigabyte/
+# Gigabyte = int(input(("Enter the Gigabyte: ")))
+# to_bytes = Gigabyte/(1024*1024*1024)
+
+# print(f"{Gigabyte}GB is {to_bytes} bytes")
+
+# program to convert Bytes to GB
+byte = int(input(("Enter the byte: ")))
+giga_bytes = byte*(1024*1024*1024)
+
+print(f"{byte}GB is {giga_bytes} bytes")
